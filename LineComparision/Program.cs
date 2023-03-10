2e11bbd98f842f851cf54af0ceac3df27e009b93
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-10 12:09:20 +0530

    [Ganesh] Add:Line comparision is added

---
+++ LineComparision/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LineComparision
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to line calculator");
+        }
+    }
+}
