108deed1f7117033479950983df50988049fc4b9
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-10 12:23:22 +0530

    [Ganesh] Add:line length is added

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -6,6 +6,16 @@ namespace LineComparision
         static void Main(String[] args)
         {
             Console.WriteLine("Welcome to line calculator");
+            Console.WriteLine("Enter x1: ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y1: ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter x2: ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y2: ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            CalculateLength calculateLength = new CalculateLength(x1, y1, x2, y2);
+            calculateLength.calculate();
         }
     }
 }
